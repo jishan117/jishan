c591a646496751a3f3fc74f14451a779a7b5cae6
jishan117 <103203992+jishan117@users.noreply.github.com>
2022-06-02 02:17:43 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Many of us,like to write the same sentence or word many times to please our lover. We do this with copy paste. But the work done through copy paste is very time consuming. Again we cannot calculate it correctly. So my little one program. If we tell it how many times a word has to be written, it will write it. Then you can copy it and send it to him.")
+var n = prompt( ' what do you want to write ' )
+var number = parseInt(prompt('how many time do you want to write'))
+
+
+for(var x=1; x<=number; x++)
+{
+document.write(' ' + ' ' + n )
+
+}
